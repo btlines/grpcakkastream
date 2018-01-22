2dbb8c6aafdfb9713e94fa5246d8a0a0920f6e1a
damien <damien@beyondthelines.net>
2018-01-22 23:52:22 +0000

    Use async callbacks in GrpcSinkStage

--- runtime/src/main/scala/grpc/akkastreams/GrpcSinkStage.scala
+++ runtime/src/main/scala/grpc/akkastreams/GrpcSinkStage.scala
@@ -1,7 +1,5 @@
 package grpc.akkastreams
 
-import java.util.concurrent.atomic.AtomicReference
-
 import akka.stream.{Attributes, Inlet, SinkShape}
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler}
 import io.grpc.stub.CallStreamObserver
@@ -11,22 +9,23 @@ class GrpcSinkStage[I](observer: CallStreamObserver[I]) extends GraphStage[SinkS
   override val shape: SinkShape[I] = SinkShape.of(in)
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) with InHandler with Runnable {
-      val element: AtomicReference[Option[I]] = new AtomicReference[Option[I]](None)
+      var element: Option[I] = None
 
-      override def run(): Unit =
-        if (observer.isReady) {
-          element.getAndSet(None).foreach { value =>
+      override def run(): Unit = getAsyncCallback((_: Unit) => {
+        element match {
+          case Some(value) if observer.isReady =>
             observer.onNext(value)
             tryPull(in)
-          }
+          case _ => ()
         }
+      }).invoke(())
 
       override def onPush(): Unit = {
         val value = grab(in)
         if (observer.isReady) {
           observer.onNext(value)
           pull(in)
-        } else element.compareAndSet(None, Some(value))
+        } else element = Some(value)
       }
 
       override def onUpstreamFinish(): Unit = observer.onCompleted()

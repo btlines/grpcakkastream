8abdf8b539b93243859cbd9b7fcb87e7fe3ac37d
damien <damien@beyondthelines.net>
2018-01-30 22:24:01 +0000

    Back-pressure for server-streaming calls

--- runtime/src/main/scala/grpc/akkastreams/GrpcAkkaStreamsClientCalls.scala
+++ runtime/src/main/scala/grpc/akkastreams/GrpcAkkaStreamsClientCalls.scala
@@ -1,9 +1,11 @@
 package grpc.akkastreams
 
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
+
 import akka.NotUsed
 import akka.stream.scaladsl.{Flow, Source}
 import com.trueaccord.scalapb.grpc.Grpc
-import io.grpc.ClientCall
+import io.grpc.{ClientCall, Metadata, Status}
 import io.grpc.stub._
 
 object GrpcAkkaStreamsClientCalls {
@@ -22,24 +24,35 @@ object GrpcAkkaStreamsClientCalls {
       new GrpcGraphStage[I, O](outputObserver => {
         val out = outputObserver.asInstanceOf[ClientResponseObserver[I, O]]
         val in = new ClientCallStreamObserver[I] {
-          override def cancel(message: String, cause: Throwable): Unit = ()
+          val halfClosed = new AtomicBoolean(false)
+          val onReadyHandler = new AtomicReference[Option[Runnable]](None)
+          val listener = new ClientCall.Listener[O] {
+            override def onClose(status: Status, trailers: Metadata): Unit =
+              out.onCompleted()
+            override def onMessage(message: O): Unit =
+              out.onNext(message)
+            override def onReady(): Unit =
+              onReadyHandler.get().foreach(_.run())
+          }
+          call.start(listener, new Metadata())
 
-          override def setOnReadyHandler(onReadyHandler: Runnable): Unit = ()
-
-          override def request(count: Int): Unit = ()
-
+          override def cancel(message: String, cause: Throwable): Unit =
+            call.cancel(message, cause)
+          override def setOnReadyHandler(onReadyHandler: Runnable): Unit =
+            this.onReadyHandler.set(Some(onReadyHandler))
+          override def request(count: Int): Unit = call.request(count)
           override def disableAutoInboundFlowControl(): Unit = ()
-
-          override def isReady: Boolean = true
-
-          override def setMessageCompression(enable: Boolean): Unit = ()
-
-          override def onError(t: Throwable): Unit = ()
-
+          override def isReady: Boolean = !halfClosed.get() || call.isReady
+          override def setMessageCompression(enable: Boolean): Unit =
+            call.setMessageCompression(enable)
+          override def onError(t: Throwable): Unit =
+            call.cancel("Cancelled by client with StreamObserver.onError()", t)
           override def onCompleted(): Unit = ()
-
-          override def onNext(request: I): Unit =
-            ClientCalls.asyncServerStreamingCall(call, request, outputObserver)
+          override def onNext(request: I): Unit = {
+            call.sendMessage(request)
+            halfClosed.set(true)
+            call.halfClose()
+          }
         }
         out.beforeStart(in)
         in
